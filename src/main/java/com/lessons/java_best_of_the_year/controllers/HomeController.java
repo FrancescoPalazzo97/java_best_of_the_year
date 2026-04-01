3defae4bf2b69665accb6920b323d86eead18038
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-04-01 13:03:54 +0200

    refactoring: refactoring getBest... methods

--- src/main/java/com/lessons/java_best_of_the_year/controllers/HomeController.java
+++ src/main/java/com/lessons/java_best_of_the_year/controllers/HomeController.java
@@ -9,10 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.lessons.java_best_of_the_year.Movie;
 import com.lessons.java_best_of_the_year.Song;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+
+    private List<Movie> getBestMovies() {
+        return List.of(new Movie("Il signore degli anelli"), new Movie("Batman"), new Movie("Dune"));
+    }
+
+    private List<Song> getBestSongs() {
+        return List.of(new Song("Aerials"), new Song("Bleed it Out"), new Song("Broken"));
+    }
 
     @GetMapping("/")
     public String welcome(Model model) {
@@ -21,34 +28,6 @@ public class HomeController {
         model.addAttribute("name", name);
 
         return "index";
-    }
-
-    private List<Movie> getBestMovies() {
-        List<Movie> movieList = new ArrayList<Movie>();
-
-        Movie movie1 = new Movie("Il signore degli anelli");
-        Movie movie2 = new Movie("Batman");
-        Movie movie3 = new Movie("Dune");
-
-        movieList.add(movie1);
-        movieList.add(movie2);
-        movieList.add(movie3);
-
-        return movieList;
-    }
-
-    private List<Song> getBestSongs() {
-        List<Song> songList = new ArrayList<>();
-
-        Song song1 = new Song("Aerials");
-        Song song2 = new Song("Bleed it Out");
-        Song song3 = new Song("Broken");
-
-        songList.add(song1);
-        songList.add(song2);
-        songList.add(song3);
-
-        return songList;
     }
 
     @GetMapping("/movies")
